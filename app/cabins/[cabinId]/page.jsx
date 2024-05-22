480669c92f98b7919286e72b89fbcb22b461cb1b
ericndungutse <dav.ndungutse@gmail.com>
2024-05-23 00:08:35 +0200

    feat: Make all pages static

--- app/cabins/[cabinId]/page.jsx
+++ app/cabins/[cabinId]/page.jsx
@@ -1,4 +1,7 @@
-import { getCabin } from '@/app/_lib/data-service';
+import {
+  getCabin,
+  getCabins,
+} from '@/app/_lib/data-service';
 import {
   EyeSlashIcon,
   MapPinIcon,
@@ -13,6 +16,16 @@ export async function generateMetadata({ params }) {
   return {
     title: `Cabin ${name}`,
   };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
+
+  return ids;
 }
 
 export default async function Page({ params }) {
