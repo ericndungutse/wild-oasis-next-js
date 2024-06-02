661d6e76bb6ddf0ffdcbfd46dba4d6cf29bef4ff
ericndungutse <dav.ndungutse@gmail.com>
2024-06-02 11:21:54 +0200

    feat: Implement signout

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,8 +1,12 @@
 'use server';
 
-import { signIn } from './auth';
+import { signIn, signOut } from './auth';
 
 export async function signInAction() {
   // Could also get form /api/providers
   await signIn('google', { redirectTo: '/account' });
+}
+
+export async function signOutAction() {
+  await signOut({ redirectTo: '/' });
 }
